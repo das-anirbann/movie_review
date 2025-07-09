5a746f165b232bce287c46080284242578b97ad3
Anirban Das <anirban140963@gmail.com>
2025-07-09 22:07:54 +0530

    second commit

---
+++ src/main/java/das/anirban/movies/movieRepository.java
@@ -0,0 +1,9 @@
+package das.anirban.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface movieRepository extends MongoRepository<Movie, ObjectId> {
+}
